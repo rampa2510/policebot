dd20481a97428016c5b69b2c2fc31ac87e39d5ec
neelb99 <neelbajaj1234@hotmail.com>
2020-01-11 13:19:45 +0530

    static files

--- server.js
+++ server.js
@@ -47,12 +47,10 @@ app.use((error, req, res) => {
   // return res.status(500).json({error:error.message});
 });
 
-if(PORT!==5000){
-  app.use(express.static(path.join(__dirname,"client","build")));
-  app.get("*",(req,res)=>{
-      res.sendFile(path.join(__dirname,"client","build","index.html"));
+app.use(express.static(path.join(__dirname,"client","build")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"client","build","index.html"));
   });
-}
 
 app.listen(PORT, async () => {
   console.log(`listening on port ${PORT}`);
