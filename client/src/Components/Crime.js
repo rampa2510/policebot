3272151825f1b9e722721443d94ad867243f524c
neelb99 <neelbajaj1234@hotmail.com>
2020-01-13 22:00:57 +0530

    displayed suspects, updated navbar styling

--- client/src/Components/Crime.js
+++ client/src/Components/Crime.js
@@ -3,6 +3,21 @@ import React from 'react';
 const Crime = props=>{
 
     const item=props["data"];
+
+    const getSuspects = ()=>{
+        if(item["personArr"].length>0)
+            return(
+                <>
+                {item["personArr"].map((suspect,index)=>{
+                    return(
+                    <li key={index}>
+                        {suspect}
+                    </li>
+                    );
+                })}
+                </>
+            );
+    }
     
     return(
         <>
@@ -10,6 +25,7 @@ const Crime = props=>{
         <p>Reported By: {item["name"]}</p>
         <p>Crime: {item["crime"]}</p>
         <p>City: {item["city"]}</p>
+        <p>Suspects: <ul>{getSuspects()}</ul></p>
         <p>Date of Crime: {item["date"].substring(8,10)+'/'+item["date"].substring(5,7)+'/'+item["date"].substring(0,4)}</p>
         <p>Status: {item["status"]}</p>
         <p>Details: {item["details"]}</p>
