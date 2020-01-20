413f1e70e809d04cb147a19181e2568233e5f615
neelb99 <neelbajaj1234@hotmail.com>
2020-01-20 22:36:23 +0530

    whatsapp otp

--- Controllers/registration.controller.js
+++ Controllers/registration.controller.js
@@ -6,6 +6,7 @@
 const hashPass = require("../Helpers/hashPassword");
 const { findOne, insertOne } = require("../Helpers/queryHandler");
 const { sign } = require("jsonwebtoken");
+const client = require('twilio')("ACd68a6040106a2b0d3ebc3d2143f1a5ba","8efb9f0856c00bd17eced4b801f2c887");
 //########################################################################################
 
 module.exports = async (req, res, next) => {
@@ -36,6 +37,13 @@ module.exports = async (req, res, next) => {
     const token = sign(userData, 'sihpolicebotsecret');
 
     res.status(201).json({ message: "Sucess", token });
+    const otp = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
+    const mobile = phone.toString()
+    client.messages.create({
+      from: 'whatsapp:+14155238886',
+      to:'whatsapp:+91'+mobile,
+      body: 'Your otp code is '+otp
+    })
     next();
   } catch (error) {
     res.status(500).json({ error });
