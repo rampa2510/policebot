790ae3ffac1280b0c250c7f29734ee8280085a46
neelb99 <neelbajaj1234@hotmail.com>
2020-02-01 18:59:58 +0530

    styling case page

--- client/src/Components/Updates.js
+++ client/src/Components/Updates.js
@@ -1,6 +1,6 @@
 import React,{useState, useEffect} from 'react';
 import intereptor from '../Services/Interceptor';
-import { Grid, Paper, makeStyles, TextField, Button, Hidden } from '@material-ui/core';
+import { Grid, Paper, makeStyles, TextField, Button, Hidden, Select, FormControl, MenuItem, InputLabel } from '@material-ui/core';
 import Crime from './Crime';
 
 const Updates = props=>{
@@ -86,30 +86,22 @@ const Updates = props=>{
 
     const useStyles = makeStyles(theme => ({
         root: {
-            marginTop:"32px",
             textAlign: "center",
-            padding:theme.spacing(2),
-            minHeight:"100vh",
             maxWidth:"100vw",
-            alignItems:"center",
         },
         field:{
             background:"#fff",
-            margin: theme.spacing(1),
-            width:"80%",
+            width:"100%",
           },
           paper: {
-            margin: theme.spacing(2),
             padding: theme.spacing(2),
             color: theme.palette.text.secondary,
+            textAlign:"center",
             },
           formButtons: {
-            width:"80%",
-            margin: theme.spacing(1),
-            marginTop: theme.spacing(3),
-            marginBottom: theme.spacing(3),
+          	height:"100%",
+          	width:"100%",
             background:"#262626",
-            padding: "12px",
             "&:hover": {
               backgroundColor: "#808080"
           }
@@ -121,34 +113,90 @@ const Updates = props=>{
     const getButtons = ()=>{
         if(caseStatus==='pending'){
             return(
-                <>
+            	<Grid container spacing={4}>
+	            	<Hidden smDown>
+	            	<Grid item md={3}/>
+	            	</Hidden>
+                    <Grid item xs={12} md={6}>
+                        <Paper className={classes.paper} elevation={3}>
+                            <Crime data={data} />
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={12} md={12} style={{marginTop:"20px", minHeight:"100vh%"}}>
+                    
+                <Grid container spacing={1} style={{width:"100%", textAlign:"center", height:"100%", }}>
+            	<Hidden>
+            		<Grid item xs={1} md={3}/>			
+            	</Hidden>
+                <Grid item xs={5} md={3}>
                 <Button variant="contained" color="primary"  className={classes.actionbuttons} onClick={()=>startInvestigation(props.currentCaseNo)}>Investigate</Button>
+                </Grid>
+                <Grid item xs={5} md={3}>
                 <Button variant="contained" color="secondary"  className={classes.actionbuttons} onClick={()=>markSpam()}>Mark As Spam</Button>
-                </>
+                </Grid>
+                </Grid>
+                </Grid>
+                </Grid>
             );
         }
         else if(caseStatus==='ongoing'){
             return(
-                <>
-                <form onSubmit={handleSubmit}>
-                    <TextField required variant="outlined" className={classes.field} value={updateMsg} onChange={e=>setUpdateMsg(e.target.value)} placeholder="Update" />
-                    
-                    <Button color="primary" variant="contained" className={classes.formButtons} type="submit">
-                        Update
-                    </Button>
+            	<Grid container spacing={4}>
+                    <Grid item xs={12} md={6}>
+                        <Paper className={classes.paper} elevation={3}>
+                            <Crime data={data} />
+                        </Paper>
+                    </Grid>
+                    <Hidden smUp>
+                    <Grid item xs={1}/>
+                    </Hidden>
+                    <Grid item xs={10} md={6} style={{marginTop:"20px", minHeight:"100vh%"}}>
+                <Grid container spacing={2} style={{minHeight:"50vh", textAlign:"center"}}>
+		                <form onSubmit={handleSubmit} style={{width:"100%"}}>
+		                	<Grid container spacing={2}>
+		                		<Grid item xs={8} md={9}>
+		                    	<TextField required variant="outlined" className={classes.field} value={updateMsg} onChange={e=>setUpdateMsg(e.target.value)} placeholder="Update" />
+		                    	</Grid>
+		                    	<Grid item xs={4} md={3}>
+		                    	<Button color="primary" variant="contained" className={classes.formButtons} type="submit">
+		                        	Update
+		                    	</Button>
+		                    	</Grid>
+		                    </Grid>
+		                </form>
+                <form onSubmit={handleTransfer} style={{width:"100%"}}>
+                	<Grid container spacing={2}>
+
+		                		<Grid item xs={8} md={9}>
+		                			<FormControl variant="outlined" style={{width:"100%"}}>
+		                			<InputLabel>Select Police Officer</InputLabel>
+                    				<Select style={{width:"100%"}} className={classes.field} required list="policemanList" value={transferTo} onChange={e=>setTransferTo(e.target.value)}>
+				                        
+				                    	{police.map((item)=><MenuItem value={item.name} >{item.name}</MenuItem>)}
+				                    </Select>
+				                    </FormControl>
+				                </Grid>
+				                <Grid item xs={4} md={3}>
+				                    <Button color="primary" variant="contained" className={classes.formButtons} type="submit">
+				                        Transfer
+				                    </Button>
+				                </Grid>
+	                </Grid>
                 </form>
-                <form onSubmit={handleTransfer}>
-                    <select required list="policemanList" value={transferTo} onChange={e=>setTransferTo(e.target.value)}>
-                        <option value="" disabled selected>Select Police Officer</option>
-                    {police.map((item)=><option value={item.name} >{item.name}</option>)}
-                    </select>
-                    <Button color="primary" variant="contained" className={classes.formButtons} type="submit">
-                        Transfer Case
-                    </Button>
-                </form>
-                <Button variant="contained" color="secondary"  className={classes.actionbuttons} onClick={()=>markSpam()}>Mark As Spam</Button>
-                <Button variant="contained" color="primary" className={classes.actionbuttons} onClick={()=>finishInvestigation(props.currentCaseNo)}>Finish Investigation</Button>
-                </>
+                <Grid container spacing={2} style={{width:"100%", textAlign:"center"}}>
+                	<Hidden>
+                		<Grid item xs={1}/>			
+                	</Hidden>
+                	<Grid item xs={5}>
+                	<Button variant="contained" color="secondary"  className={classes.actionbuttons} onClick={()=>markSpam()}>Mark Spam</Button>
+                	</Grid>
+                	<Grid item xs={5}>
+                	<Button variant="contained" color="primary" className={classes.actionbuttons} onClick={()=>finishInvestigation(props.currentCaseNo)}>Complete</Button>
+                	</Grid>
+                	</Grid>
+               	</Grid>
+               	</Grid>
+                </Grid>
             );
         }
     }
@@ -170,25 +218,9 @@ const Updates = props=>{
     const loadPage = ()=>{
         if(loaded){
         return(
-                <Grid className={classes.root} container spacing={0}>
-                    <Hidden smDown>
-                        <Grid item md={3}></Grid>
-                    </Hidden>
-                    <Grid item xs={12} md={6}>
-                    {getButtons()}
-                    </Grid>
-                    <Hidden smDown>
-                        <Grid item md={3}></Grid>
-                    </Hidden>
-                    <Hidden smDown>
-                        <Grid item md={3} ></Grid>
-                    </Hidden>
-                    <Grid item xs={12} md={6}>
-                        <Paper className={classes.paper} elevation={3}>
-                            <Crime data={data} />
-                        </Paper>
-                    </Grid>
-                </Grid>
+               <> 
+                    	{getButtons()}
+                   </>
             );
         }
     }
